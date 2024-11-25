0df9f975e455ee7dcd0a4184464f8ef230ed226e
miagj02 <mjosef24@student.aau.dk>
2024-11-25 09:55:22 +0100

    test hej

---
+++ hejnej.c
@@ -0,0 +1,7 @@
+#include "hejnej.h"
+#include <stdio.h>
+
+int main(void){
+  printf("Hello world");
+
+  }
---
+++ hejnej.h
@@ -0,0 +1,6 @@
+#ifndef HEJNEJ_H
+#define HEJNEJ_H
+
+#include "hejnej.c"
+
+#endif //HEJNEJ_H
